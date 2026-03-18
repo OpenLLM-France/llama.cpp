d89fc0762ea183fc0bab0b210f8ca48b9a81bf69
Jeronymous <jeromelouradour@gmail.com>
2026-03-18 17:16:48 +0100

    fix and update test

--- tests/test-tokenizer-random.py
+++ tests/test-tokenizer-random.py
@@ -30,7 +30,7 @@ class LibLlama:
 
     DEFAULT_PATH_LLAMA_H = "./include/llama.h"
     DEFAULT_PATH_INCLUDES = ["./ggml/include/", "./include/"]
-    DEFAULT_PATH_LIBLLAMA = "./build/src/libllama.so"  # CMakeLists.txt: BUILD_SHARED_LIBS ON
+    DEFAULT_PATH_LIBLLAMA = "./build/bin/libllama.so"  # CMakeLists.txt: BUILD_SHARED_LIBS ON
 
     def __init__(self, path_llama_h: str | None = None, path_includes: list[str] = [], path_libllama: str | None = None):
         path_llama_h = path_llama_h or self.DEFAULT_PATH_LLAMA_H
@@ -79,6 +79,9 @@ class LibLlamaModel:
         self.model = self.lib.llama_model_load_from_file(path_model.encode(), mparams)
         if not self.model:
             raise RuntimeError("error: failed to load model '%s'" % path_model)
+        self.vocab = self.lib.llama_model_get_vocab(self.model)
+        if not self.vocab:
+            raise RuntimeError("error: failed to get vocab for model '%s'" % path_model)
         if isinstance(cparams, dict):
             cparams = libllama.context_default_params(**cparams)
         self.ctx = self.lib.llama_new_context_with_model(self.model, cparams)
@@ -99,10 +102,10 @@ class LibLlamaModel:
 
     def tokenize(self, text: str, add_special: bool = False, parse_special: bool = False) -> list[int]:
         encoded_text: bytes = text.encode("utf-8")
-        num = self.lib.llama_tokenize(self.model, encoded_text, len(encoded_text), self.token_ids, len(self.token_ids), add_special, parse_special)
+        num = self.lib.llama_tokenize(self.vocab, encoded_text, len(encoded_text), self.token_ids, len(self.token_ids), add_special, parse_special)
         while num < 0 and len(self.token_ids) < (16 << 20):
             self.token_ids = self.ffi.new("llama_token[]", -2 * num)
-            num = self.lib.llama_tokenize(self.model, encoded_text, len(encoded_text), self.token_ids, len(self.token_ids), add_special, parse_special)
+            num = self.lib.llama_tokenize(self.vocab, encoded_text, len(encoded_text), self.token_ids, len(self.token_ids), add_special, parse_special)
         return list(self.token_ids[0:num])
 
     def detokenize(self, ids: list[int], remove_special: bool = False, unparse_special: bool = False) -> str:
@@ -110,10 +113,10 @@ class LibLlamaModel:
             self.token_ids = self.ffi.new("llama_token[]", 2 * len(ids))
         for i, id in enumerate(ids):
             self.token_ids[i] = id
-        num = self.lib.llama_detokenize(self.model, self.token_ids, len(ids), self.text_buff, len(self.text_buff), remove_special, unparse_special)
+        num = self.lib.llama_detokenize(self.vocab, self.token_ids, len(ids), self.text_buff, len(self.text_buff), remove_special, unparse_special)
         while num < 0 and len(self.text_buff) < (16 << 20):
             self.text_buff = self.ffi.new("uint8_t[]", -2 * num)
-            num = self.lib.llama_detokenize(self.model, self.token_ids, len(ids), self.text_buff, len(self.text_buff), remove_special, unparse_special)
+            num = self.lib.llama_detokenize(self.vocab, self.token_ids, len(ids), self.text_buff, len(self.text_buff), remove_special, unparse_special)
         return str(cast(Buffer, self.ffi.buffer(self.text_buff, num)), encoding="utf-8", errors="replace")  # replace errors with '\uFFFD'
 
 
@@ -152,6 +155,9 @@ class TokenizerGroundtruth (Tokenizer):
 
     def decode(self, ids: list[int]) -> str:
         return self.model.decode(ids, skip_special_tokens=False)
+    
+    def convert_ids_to_tokens(self, ids: list[int]) -> list[str]:
+        return self.model.convert_ids_to_tokens(ids)
 
 
 class TokenizerLlamaCpp (Tokenizer):
@@ -204,6 +210,12 @@ def generator_custom_text() -> Iterator[str]:
         "\n =",
         "' era",
         "Hello, y'all! How are you 😁 ?我想在apple工作1314151天～",
+    ]
+
+
+def generator_digit() -> Iterator[str]:
+    """Digits"""
+    yield from [
         "3",
         "33",
         "333",
@@ -213,6 +225,20 @@ def generator_custom_text() -> Iterator[str]:
         "3333333",
         "33333333",
         "333333333",
+        "333333333+333",
+    ]
+
+
+def generator_contractions() -> Iterator[str]:
+    """Contractions and apostrophes"""
+    yield from [
+        "I'll",
+        "We've they're",
+        "Bonjour quoiqu'aujourd'hui",
+        "puisqu'après",
+        "j're",
+        "“Bonjour quoiqu'aujourd'hui”",
+        "puisqu’après",
     ]
 
 
@@ -418,7 +444,7 @@ def compare_tokenizers(tokenizer1: TokenizerGroundtruth, tokenizer2: TokenizerLl
         return min(len(ids1), len(ids2))
 
     def check_detokenizer(text: str, text1: str, text2: str) -> bool:
-        if text1 == text2:  # equal to TokenizerGroundtruth?
+        if text1 == text2 or text2 == text:  # equal to TokenizerGroundtruth?
             return True
         # equal to source text?
         if tokenizer1.add_bos_token and tokenizer1.bos_token and isinstance(tokenizer1.bos_token, str):  # remove BOS
@@ -436,7 +462,7 @@ def compare_tokenizers(tokenizer1: TokenizerGroundtruth, tokenizer2: TokenizerLl
     t_start = time.perf_counter()
     encode_errors = 0
     decode_errors = 0
-    MAX_ERRORS = 10
+    MAX_ERRORS = 20
 
     logger.info("%s: %s" % (generator.__qualname__, "ini"))
     for text in generator:
@@ -455,23 +481,30 @@ def compare_tokenizers(tokenizer1: TokenizerGroundtruth, tokenizer2: TokenizerLl
         t_encode2 += t2 - t1
         t_decode1 += t3 - t2
         t_decode2 += t4 - t3
-        if encode_errors < MAX_ERRORS and ids1 != ids2:
+        had_error = False
+        if (MAX_ERRORS is None or encode_errors < MAX_ERRORS) and ids1 != ids2:
             i = find_first_mismatch(ids1, ids2)
-            ids1 = list(ids1)[max(0, i - 2) : i + 5 + 1]
-            ids2 = list(ids2)[max(0, i - 2) : i + 5 + 1]
-            logger.error(" Expected: " + str(ids1))
-            logger.error("   Result: " + str(ids2))
+            ids1_ctx = list(ids1)[max(0, i - 2) : i + 5 + 1]
+            ids2_ctx = list(ids2)[max(0, i - 2) : i + 5 + 1]
+            logger.error(f"  Input: {repr(text[:100])}")
+            logger.error(" Expected: " + str(ids1_ctx) + "  " + str(tokenizer1.convert_ids_to_tokens(ids1_ctx)))
+            logger.error("   Result: " + str(ids2_ctx) + "  " + str(tokenizer1.convert_ids_to_tokens(ids2_ctx)))
             encode_errors += 1
-            logger.error(f" {encode_errors=}")
-        if decode_errors < MAX_ERRORS and not check_detokenizer(text, text1, text2):
+            # logger.error(f" {encode_errors=}")
+            had_error = True
+        if (MAX_ERRORS is None or decode_errors < MAX_ERRORS) and not check_detokenizer(text, text1, text2):
             i = find_first_mismatch(text1, text2)
-            text1 = list(text1[max(0, i - 2) : i + 5 + 1])
-            text2 = list(text2[max(0, i - 2) : i + 5 + 1])
-            logger.error(" Expected: " + " ".join(hex(ord(x)) for x in text1))
-            logger.error("   Result: " + " ".join(hex(ord(x)) for x in text2))
+            text1_ctx = text1[max(0, i - 2) : i + 5 + 1]
+            text2_ctx = text2[max(0, i - 2) : i + 5 + 1]
+            logger.error(f"  Input: {repr(text[:100])}")
+            logger.error(" Expected: " + repr(text1_ctx))
+            logger.error("   Result: " + repr(text2_ctx))
             decode_errors += 1
-            logger.error(f" {decode_errors=}")
-        if encode_errors >= MAX_ERRORS and decode_errors >= MAX_ERRORS:
+            # logger.error(f" {decode_errors=}")
+            had_error = True
+        if had_error:
+            logger.error("")
+        if MAX_ERRORS is not None and encode_errors >= MAX_ERRORS and decode_errors >= MAX_ERRORS:
             logger.error(f" EXIT: {encode_errors=} {decode_errors=}")
             # raise Exception()
             break
@@ -493,74 +526,76 @@ def main(argv: list[str] | None = None):
     tokenizer1 = TokenizerGroundtruth(args.dir_tokenizer)
     tokenizer2 = TokenizerLlamaCpp(args.vocab_file)
 
-    # compare_tokenizers(tokenizer1, tokenizer2, generator_custom_text())
-    # compare_tokenizers(tokenizer1, tokenizer2, generator_custom_text_edge_cases())
+    compare_tokenizers(tokenizer1, tokenizer2, generator_custom_text())
+    compare_tokenizers(tokenizer1, tokenizer2, generator_digit())
+    compare_tokenizers(tokenizer1, tokenizer2, generator_contractions())
+    compare_tokenizers(tokenizer1, tokenizer2, generator_custom_text_edge_cases())
     compare_tokenizers(tokenizer1, tokenizer2, generator_ascii_lr_strip())
     compare_tokenizers(tokenizer1, tokenizer2, generator_apostrophe())
     compare_tokenizers(tokenizer1, tokenizer2, generator_unicodes())
     compare_tokenizers(tokenizer1, tokenizer2, generator_vocab_words(tokenizer1))
     compare_tokenizers(tokenizer1, tokenizer2, generator_added_lr_strip(tokenizer1))
-    # compare_tokenizers(tokenizer1, tokenizer2, generator_random_added_tokens(tokenizer1, 10_000))
-    # compare_tokenizers(tokenizer1, tokenizer2, generator_random_chars(10_000))
-    # compare_tokenizers(tokenizer1, tokenizer2, generator_random_unicodes(10_000))
-    # compare_tokenizers(tokenizer1, tokenizer2, generator_random_vocab_chars(tokenizer1, 10_000))
-    # compare_tokenizers(tokenizer1, tokenizer2, generator_random_vocab_words(tokenizer1, 5_000))
+    compare_tokenizers(tokenizer1, tokenizer2, generator_random_added_tokens(tokenizer1, 10_000))
+    compare_tokenizers(tokenizer1, tokenizer2, generator_random_chars(10_000))
+    compare_tokenizers(tokenizer1, tokenizer2, generator_random_unicodes(10_000))
+    compare_tokenizers(tokenizer1, tokenizer2, generator_random_vocab_chars(tokenizer1, 10_000))
+    compare_tokenizers(tokenizer1, tokenizer2, generator_random_vocab_words(tokenizer1, 5_000))
 
     tokenizer2.model.free()
 
 
 if __name__ == "__main__":
-    # main()
+    main()
 
-    if True:
-        logging.basicConfig(
-            level    = logging.DEBUG,
-            format   = "%(asctime)s.%(msecs)03d %(name)s %(levelname)s %(message)s",
-            datefmt  = "%Y-%m-%d %H:%M:%S",
-            filename = logger.name + ".log",
-            filemode = "a"
-        )
-    logging.basicConfig(
-        level    = logging.DEBUG,
-        format   = "%(levelname)s %(message)s",
-    )
+    # if True:
+    #     logging.basicConfig(
+    #         level    = logging.DEBUG,
+    #         format   = "%(asctime)s.%(msecs)03d %(name)s %(levelname)s %(message)s",
+    #         datefmt  = "%Y-%m-%d %H:%M:%S",
+    #         filename = logger.name + ".log",
+    #         filemode = "a"
+    #     )
+    # logging.basicConfig(
+    #     level    = logging.DEBUG,
+    #     format   = "%(levelname)s %(message)s",
+    # )
 
-    path_tokenizers   = Path("./models/tokenizers/")
-    path_vocab_format = "./models/ggml-vocab-%s.gguf"
+    # path_tokenizers   = Path("./models/tokenizers/")
+    # path_vocab_format = "./models/ggml-vocab-%s.gguf"
 
-    tokenizers = [
-        "llama-spm",      # SPM
-        "phi-3",          # SPM
-        "gemma",          # SPM
-        "gemma-2",        # SPM
-        "baichuan",       # SPM
-        "bert-bge",       # WPM
-        "jina-v2-en",     # WPM
-        "llama-bpe",      # BPE
-        "phi-2",          # BPE
-        "deepseek-llm",   # BPE
-        "deepseek-coder", # BPE
-        "falcon",         # BPE
-        "mpt",            # BPE
-        "starcoder",      # BPE
-        "gpt-2",          # BPE
-        "stablelm2",      # BPE
-        "refact",         # BPE
-        "qwen2",          # BPE
-        "olmo",           # BPE
-        "jina-v2-es",     # BPE
-        "jina-v2-de",     # BPE
-        "smaug-bpe",      # BPE
-        "poro-chat",      # BPE
-        "jina-v2-code",   # BPE
-        "viking",         # BPE
-        "jais",           # BPE
-    ]
+    # tokenizers = [
+    #     "llama-spm",      # SPM
+    #     "phi-3",          # SPM
+    #     "gemma",          # SPM
+    #     "gemma-2",        # SPM
+    #     "baichuan",       # SPM
+    #     "bert-bge",       # WPM
+    #     "jina-v2-en",     # WPM
+    #     "llama-bpe",      # BPE
+    #     "phi-2",          # BPE
+    #     "deepseek-llm",   # BPE
+    #     "deepseek-coder", # BPE
+    #     "falcon",         # BPE
+    #     "mpt",            # BPE
+    #     "starcoder",      # BPE
+    #     "gpt-2",          # BPE
+    #     "stablelm2",      # BPE
+    #     "refact",         # BPE
+    #     "qwen2",          # BPE
+    #     "olmo",           # BPE
+    #     "jina-v2-es",     # BPE
+    #     "jina-v2-de",     # BPE
+    #     "smaug-bpe",      # BPE
+    #     "poro-chat",      # BPE
+    #     "jina-v2-code",   # BPE
+    #     "viking",         # BPE
+    #     "jais",           # BPE
+    # ]
 
-    logger.info("=" * 50)
-    for tokenizer in tokenizers:
-        logger.info("-" * 50)
-        logger.info(f"TOKENIZER: '{tokenizer}'")
-        vocab_file = Path(path_vocab_format % tokenizer)
-        dir_tokenizer = path_tokenizers / tokenizer
-        main([str(vocab_file), str(dir_tokenizer), "--verbose"])
+    # logger.info("=" * 50)
+    # for tokenizer in tokenizers:
+    #     logger.info("-" * 50)
+    #     logger.info(f"TOKENIZER: '{tokenizer}'")
+    #     vocab_file = Path(path_vocab_format % tokenizer)
+    #     dir_tokenizer = path_tokenizers / tokenizer
+    #     main([str(vocab_file), str(dir_tokenizer), "--verbose"])
